bf73819145cc90fd6ef8d70b2b7b7cbe27455764
Kamil Dawid <koxxor12@gmail.com>
2021-08-19 23:10:10 +0200

    JDP210801-1 - wstepna implementacja endpointow.

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,24 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/product")
 public class ProductController {
+
+    @GetMapping
+    public void get(){}
+
+    @GetMapping({"/{id}"})
+    public void get(@PathVariable Long id){}
+
+    @PostMapping
+    public void create(){}
+
+    @PutMapping("/{product}")
+    public void update(@PathVariable String product){}
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){}
+
 }
