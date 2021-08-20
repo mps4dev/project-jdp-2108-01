ea7d0648554294d7d8a3f41da7d032fa4b7a49fe
Anna Dyrduł <ania.dyrdul1905@gmail.com>
2021-08-20 21:56:45 +0200

    task-4 OrderController - wstepna implementacja endpointow

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,4 +1,37 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/order")
 public class OrderController {
+
+    @GetMapping
+    public List<OrderDto> get() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping("/{id}")
+    public OrderDto get(@PathVariable Long id) {
+        return new OrderDto();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+
+    }
+
+    @PutMapping
+    public OrderDto update(@RequestBody OrderDto orderDto) {
+        return new OrderDto();
+    }
+
+    @PostMapping
+    public OrderDto create(@RequestBody OrderDto orderDto) {
+        return new OrderDto();
+    }
 }
