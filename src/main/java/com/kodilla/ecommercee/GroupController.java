d779acadb622403fa79f7c2417cba9959d1e5cfa
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-08-20 15:47:56 +0200

    Task-2 Wstepna implementacja endpointow GroupController

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,37 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.GroupDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/group")
 public class GroupController {
+
+    @GetMapping
+    public List<GroupDto> get() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "/{groupId}")
+    public GroupDto get(@PathVariable Long groupId) {
+        return new GroupDto();
+    }
+
+    @PostMapping
+    public void add(@RequestBody GroupDto groupDto) {
+
+    }
+
+    @PutMapping
+    public GroupDto update(@RequestBody GroupDto groupDto) {
+        return groupDto;
+    }
+
+    @DeleteMapping
+    public void delete(@RequestBody GroupDto groupDto) {
+
+    }
 }
