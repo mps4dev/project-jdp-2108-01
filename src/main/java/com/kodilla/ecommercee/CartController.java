db9d3fa2c9e596031ea0b01cd51af726dea8eb53
DominikBernatowicz <82055376+DominikBernatowicz@users.noreply.github.com>
2021-08-21 10:31:16 +0200

    Task-3 CartController - wstepna implementacja endpointow

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,4 +1,37 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.dto.CartDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/cart")
 public class CartController {
+
+    @GetMapping
+    public List<CartDto> get() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping("/{id}")
+    public CartDto get(@PathVariable Long id) {
+        return new CartDto();
+    }
+
+    @PutMapping
+    public CartDto update(@RequestBody CartDto cartDto) {
+        return new CartDto();
+    }
+
+    @PostMapping
+    public CartDto create(@RequestBody CartDto cartDto) {
+        return new CartDto();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+
+    }
 }
