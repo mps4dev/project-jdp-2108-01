c9ec64e69add441f1d1c6bb659254866f45c9917
DominikBernatowicz <82055376+DominikBernatowicz@users.noreply.github.com>
2021-08-23 17:34:50 +0200

    Poprawienie implementacji w CartController, przeniesienie do pakietu controller

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,18 +1,16 @@
-package com.kodilla.ecommercee;
+package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.dto.CartDto;
+import com.kodilla.ecommercee.dto.OrderDto;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/cart")
 public class CartController {
 
-    @GetMapping
-    public List<CartDto> get() {
-        return new ArrayList<>();
+    @PostMapping
+    public CartDto create(@RequestBody CartDto cartDto) {
+        return new CartDto();
     }
 
     @GetMapping("/{id}")
@@ -25,13 +23,13 @@ public class CartController {
         return new CartDto();
     }
 
-    @PostMapping
-    public CartDto create(@RequestBody CartDto cartDto) {
-        return new CartDto();
+    @GetMapping("/order")
+    public OrderDto order(@RequestBody CartDto cartDto) {
+        return new OrderDto();
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    public void delete(@PathVariable Long id){
 
     }
 }
