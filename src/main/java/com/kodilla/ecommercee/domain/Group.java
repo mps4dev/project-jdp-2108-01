fcdf8edc874f39cda08bd90945786f3f7266b4b8
Kamil <kamil.krol@gmail.com>
2021-08-23 14:12:42 +0200

    Removing unnecessary code from class Group

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,32 +1,10 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Entity(name = "GROUPS")
+@Entity
 public class Group {
 
     @Id
-    @GeneratedValue
     private long id;
-
-    @NotNull
-    private String name;
-
-    @OneToMany(
-            targetEntity = Product.class,
-            mappedBy = "group",
-            cascade = CascadeType.ALL,
-            fetch = FetchType.LAZY
-    )
-    private List<Product> products = new ArrayList<>();
 }
