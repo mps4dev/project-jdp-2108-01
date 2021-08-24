fae2fbb81ce12a27323d2e3fa80519cd2779b376
DominikBernatowicz <82055376+DominikBernatowicz@users.noreply.github.com>
2021-08-25 00:28:39 +0200

    dodanie CartRepository

---
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -0,0 +1,13 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.Cart;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+public interface CartRepository extends CrudRepository<Cart, Long> {
+
+}
