ed248114a8f1e46c32c7f244dd6762a60ec55834
JuliaSidor811 <juliasidor811@gmail.com>
2021-08-25 13:50:34 +0200

    poprawa task-5

---
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.kodilla.ecommercee.controller;
+
+import com.kodilla.ecommercee.dto.UserDto;
+import com.kodilla.ecommercee.dto.UserKeyDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/user")
+public class UserController {
+
+    @PostMapping
+    public UserDto create(@RequestBody UserDto userDto) {
+        return userDto;
+    }
+
+    @PutMapping("/{id}")
+    public void block(@PathVariable Long id) {
+
+    }
+    @PutMapping("/{id}")
+    public void generateKey(@PathVariable Long id) {
+
+    }
+
+
+}
---
+++ src/main/java/com/kodilla/ecommercee/dto/UserKeyDto.java
@@ -0,0 +1,10 @@
+package com.kodilla.ecommercee.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserKeyDto {
+    private String value;
+}
