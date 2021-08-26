ddbdd56789c4a17b4208a1d472035dc71d5ac3e7
JuliaSidor811 <juliasidor811@gmail.com>
2021-08-26 19:51:15 +0200

    poprawa task-15

---
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -0,0 +1,30 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity(name = "USERS")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private boolean status;
+
+    @NotNull
+    private long userKey;
+}
--- src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.kodilla.ecommercee.repository;
 
+import com.kodilla.ecommercee.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
