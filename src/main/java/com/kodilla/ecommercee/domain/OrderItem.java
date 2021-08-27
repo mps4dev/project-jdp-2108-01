dedcc591f02cd033551172bc1654fbe40a5b0e02
Patryk Ziemniak <86192797+patryk-ziemniak@users.noreply.github.com>
2021-08-27 17:10:11 +0200

    task-27 Wprowadzenie kwestii ujednoliconych na spotkaniu 26.08 (#16)
    * Changed imports with * into separate imports and unified formatting of code
    
    * Deleted @Transactional from repository controllers
    
    * Changed deleting method in GroupController to do it by groupId, not GroupDto
    
    * Changes usage of Long into long in Controllers and Domain objects
    
    * Changed naming in entities to match the default naming by Hibernate

--- src/main/java/com/kodilla/ecommercee/domain/OrderItem.java
+++ src/main/java/com/kodilla/ecommercee/domain/OrderItem.java
@@ -2,14 +2,10 @@ package com.kodilla.ecommercee.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 
 @Entity
-public class OrderItem {
+public class User {
 
     @Id
     private long id;
-
-    @ManyToOne
-    private Cart cart;
 }
