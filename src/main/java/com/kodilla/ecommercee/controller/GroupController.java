732ef93ec81f6c6f3c710bea80c4213d29163397
Patryk Ziemniak <86192797+patryk-ziemniak@users.noreply.github.com>
2021-08-27 17:10:11 +0200

    task-27 Wprowadzenie kwestii ujednoliconych na spotkaniu 26.08 (#16)
    * Changed imports with * into separate imports and unified formatting of code
    
    * Deleted @Transactional from repository controllers
    
    * Changed deleting method in GroupController to do it by groupId, not GroupDto
    
    * Changes usage of Long into long in Controllers and Domain objects
    
    * Changed naming in entities to match the default naming by Hibernate

---
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -0,0 +1,44 @@
+package com.kodilla.ecommercee.controller;
+
+import com.kodilla.ecommercee.dto.GroupDto;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/group")
+public class GroupController {
+
+    @GetMapping
+    public List<GroupDto> get() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping("/{id}")
+    public GroupDto get(@PathVariable long id) {
+        return new GroupDto();
+    }
+
+    @PostMapping
+    public GroupDto create(@RequestBody GroupDto groupDto) {
+        return groupDto;
+    }
+
+    @PutMapping
+    public GroupDto update(@RequestBody GroupDto groupDto) {
+        return groupDto;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+
+    }
+}
--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,39 +1,11 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import java.util.ArrayList;
-import java.util.List;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Entity(name = "CARTS")
+@Entity
 public class Cart {
 
     @Id
-    @GeneratedValue
     private long id;
-
-    @OneToMany(
-            targetEntity = OrderItem.class,
-            mappedBy = "cart",
-            cascade = CascadeType.ALL,
-            fetch = FetchType.LAZY
-    )
-    private List<OrderItem> items = new ArrayList<>();
-
-    @OneToOne
-    @JoinColumn(name = "ORDER_ID")
-    private Order order;
 }
