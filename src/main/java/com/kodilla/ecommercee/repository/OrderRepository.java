5aaf61468332f88d03199f7f5c850722e3cbbbea
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-08-27 15:38:32 +0200

    Deleted @Transactional from repository controllers

--- src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
 }
