55402b5a42dafda92eb8b2bac8150e66fd7ecbea
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-08-27 16:32:42 +0200

    Created implemetation of GroupDto

--- src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
@@ -1,4 +1,19 @@
 package com.kodilla.ecommercee.dto;
 
+import com.kodilla.ecommercee.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class GroupDto {
+
+    private long id;
+    private String name;
+    private List<Product> products = new ArrayList<>();
 }
