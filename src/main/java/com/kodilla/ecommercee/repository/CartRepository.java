880263275dafa8834f65772e40e2b799d2837052
DominikBernatowicz <82055376+DominikBernatowicz@users.noreply.github.com>
2021-08-28 16:29:52 +0200

    usunięcie @Transactional

--- src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -4,9 +4,6 @@ import com.kodilla.ecommercee.domain.Cart;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
-
-@Transactional
 @Repository
 public interface CartRepository extends CrudRepository<Cart, Long> {
 
