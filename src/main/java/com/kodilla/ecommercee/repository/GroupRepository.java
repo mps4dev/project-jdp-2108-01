56e8a521366ff21fd358ec31131c34200ab1631b
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-08-29 00:41:09 +0200

    Deleted useless import

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -3,7 +3,6 @@ package com.kodilla.ecommercee.repository;
 import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
