d0c20e220567c1550f9d879b4f98bb42d212ddba
DominikBernatowicz <82055376+DominikBernatowicz@users.noreply.github.com>
2021-08-28 19:35:45 +0200

    usunięcie zbędnych importów

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -9,9 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import java.util.ArrayList;
 import java.util.List;
 
