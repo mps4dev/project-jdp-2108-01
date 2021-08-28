d5db8c524716f8d298c9e940f7eee2ea008ccfe3
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-08-29 00:11:12 +0200

    Exceptions constructor improvement

--- src/main/java/com/kodilla/ecommercee/exception/CreatingObjectWithIdException.java
+++ src/main/java/com/kodilla/ecommercee/exception/CreatingObjectWithIdException.java
@@ -3,6 +3,6 @@ package com.kodilla.ecommercee.exception;
 public class CreatingObjectWithIdException extends Exception {
 
     public CreatingObjectWithIdException(Class<?> clazz, long id) {
-        super("Tried to create entity " + clazz.getSimpleName() + " with given id: " + id + ".");
+        super(String.format("Tried to create entity %s with given id: %d.", clazz.getSimpleName(), id));
     }
 }
--- src/main/java/com/kodilla/ecommercee/exception/EntityNotFoundException.java
+++ src/main/java/com/kodilla/ecommercee/exception/EntityNotFoundException.java
@@ -3,6 +3,6 @@ package com.kodilla.ecommercee.exception;
 public class EntityNotFoundException extends Exception {
 
     public EntityNotFoundException(Class<?> clazz, long id) {
-        super("Entity " + clazz.getSimpleName() + " with id: " + id + " not found.");
+        super(String.format("Entity %s with id: %d not found.", clazz.getSimpleName(), id));
     }
 }
