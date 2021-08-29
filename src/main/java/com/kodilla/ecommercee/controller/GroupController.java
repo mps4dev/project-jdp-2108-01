356f9cecc461d9cec300ff4f0f1bb469bc948957
Patryk Ziemniak <86192797+patryk-ziemniak@users.noreply.github.com>
2021-08-29 14:27:03 +0200

    task-22 GroupController - rzeczywista implementacja metod (#17)
    * Created implemetation of GroupDto
    
    * Created GroupMapper which extend abstract class EntityMapper (for use in every Mapper)
    
    * Created GroupService implementation
    
    * Created 2 exceptions used in GroupService and made them able to use in other entities' Controller implementations
    
    * Implemented fully functional methods in GroupController

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -1,6 +1,10 @@
 package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.dto.GroupDto;
+import com.kodilla.ecommercee.exception.EntityNotFoundException;
+import com.kodilla.ecommercee.exception.CreatingObjectWithIdException;
+import com.kodilla.ecommercee.service.GroupService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,35 +14,37 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("v1/group")
 public class GroupController {
 
+    private final GroupService service;
+
     @GetMapping
     public List<GroupDto> get() {
-        return new ArrayList<>();
+        return service.getAll();
     }
 
     @GetMapping("/{id}")
-    public GroupDto get(@PathVariable long id) {
-        return new GroupDto();
+    public GroupDto get(@PathVariable long id) throws EntityNotFoundException {
+        return service.getById(id);
     }
 
     @PostMapping
-    public GroupDto create(@RequestBody GroupDto groupDto) {
-        return groupDto;
+    public GroupDto create(@RequestBody GroupDto groupDto) throws CreatingObjectWithIdException {
+        return service.create(groupDto);
     }
 
     @PutMapping
-    public GroupDto update(@RequestBody GroupDto groupDto) {
-        return groupDto;
+    public GroupDto update(@RequestBody GroupDto groupDto) throws EntityNotFoundException {
+        return service.update(groupDto);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable long id) {
-
+    public void delete(@PathVariable long id) throws EntityNotFoundException {
+        service.delete(id);
     }
 }
