756110cb318ec03b6eaaa8b5c46c869867401f6e
Typhoonic <81630895+Typhoonic@users.noreply.github.com>
2021-09-02 20:19:31 +0200

    task-5 Wstepna implementacja endpointow

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -2,7 +2,12 @@ package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.dto.UserDto;
 import com.kodilla.ecommercee.dto.UserKeyDto;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/user")
@@ -15,12 +20,10 @@ public class UserController {
 
     @PutMapping("/block/{id}")
     public void block(@PathVariable long id) {
-
     }
+
     @PutMapping("/generate/{id}")
     public UserKeyDto generateKey(@PathVariable long id) {
         return new UserKeyDto();
-
     }
-
 }
