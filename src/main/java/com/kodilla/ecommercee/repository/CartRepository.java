9bd961c29c2c018e619c206dd3e53bd2da8cd45f
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-09-06 16:58:27 +0200

    Added CartRepository

---
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -0,0 +1,14 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.Cart;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartRepository extends CrudRepository<Cart, Long> {
+
+    @Override
+    List<Cart> findAll();
+}
