aa973d96e9857f8bb3e9fb22ac2eda9a99ba87c1
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-09-06 17:07:44 +0200

    Fixed Entity Mappers for new changes in DTOs

--- src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
@@ -1,18 +1,31 @@
 package com.kodilla.ecommercee.mapper;
 
 import com.kodilla.ecommercee.domain.Group;
+import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.dto.GroupDto;
+import com.kodilla.ecommercee.repository.ProductRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
 @Service
 public class GroupMapper extends EntityMapper<Group, GroupDto> {
+
+    ProductRepository repository;
 
     @Override
     public Group toEntity(final GroupDto groupDto) {
         return new Group(
                 groupDto.getId(),
                 groupDto.getName(),
-                groupDto.getProducts()
+                groupDto.getProductsIds().stream()
+                        .map(repository::findById)
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .collect(Collectors.toList())
         );
     }
 
@@ -21,7 +34,9 @@ public class GroupMapper extends EntityMapper<Group, GroupDto> {
         return new GroupDto(
                 group.getId(),
                 group.getName(),
-                group.getProducts()
+                group.getProducts().stream()
+                        .map(Product::getId)
+                        .collect(Collectors.toList())
         );
     }
 }
