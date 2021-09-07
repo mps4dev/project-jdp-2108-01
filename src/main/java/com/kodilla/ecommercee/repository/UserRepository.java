3a363ebecf384528cc6b6d5209e6f93ef970705d
Typhoonic <81630895+Typhoonic@users.noreply.github.com>
2021-09-07 22:55:55 +0200

    Delete old UserController

---
+++ src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
