d8773689856708876c55530a1502712603686ecd
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-09-07 17:16:32 +0200

    Made Cart constructors to be able to test User entity

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,11 +1,15 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Entity
 public class Cart {
