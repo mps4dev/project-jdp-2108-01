8e874d2d955c7565142e15706e2b050cc61870a9
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-09-09 14:07:03 +0200

    UserRepository findAll Override

--- src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.kodilla.ecommercee.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    @Override
+    List<User> findAll();
 }
