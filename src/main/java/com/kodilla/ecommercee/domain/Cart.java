b2d06e21cefc39cbd13e2f752fedfab8b290a358
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-09-09 15:57:38 +0200

    Reverted changes in Cart

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,15 +1,11 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Entity
 public class Cart {
