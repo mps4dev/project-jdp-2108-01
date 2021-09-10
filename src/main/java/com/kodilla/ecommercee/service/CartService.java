04ad65fc4e77caefdd7e19324a3421fb28c19d6f
DominikBernatowicz <82055376+DominikBernatowicz@users.noreply.github.com>
2021-09-10 14:22:47 +0200

    poprawienie tworzenia order

--- src/main/java/com/kodilla/ecommercee/service/CartService.java
+++ src/main/java/com/kodilla/ecommercee/service/CartService.java
@@ -7,7 +7,9 @@ import com.kodilla.ecommercee.dto.OrderDto;
 import com.kodilla.ecommercee.exception.CreatingObjectWithIdException;
 import com.kodilla.ecommercee.exception.EntityNotFoundException;
 import com.kodilla.ecommercee.mapper.CartMapper;
+import com.kodilla.ecommercee.mapper.OrderMapper;
 import com.kodilla.ecommercee.repository.CartRepository;
+import com.kodilla.ecommercee.repository.OrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +20,8 @@ public class CartService {
 
     private final CartMapper mapper;
     private final CartRepository repository;
-
+    private final OrderMapper orderMapper;
+    private final OrderRepository orderRepository;
 
     @Transactional
     public CartDto create(final CartDto cartDto) throws CreatingObjectWithIdException {
@@ -39,9 +42,10 @@ public class CartService {
     }
 
     @Transactional
-    public OrderDto order(CartDto cartDto) {
-        Cart cart = mapper.toEntity(cartDto);
-        return new OrderDto(cartDto.getId(), cartDto.getUserId(), cart.getId());
+    public OrderDto order(CartDto cartDto){
+        Cart cart = repository.save(mapper.toEntity(cartDto));
+        Order order = new Order(0, cart.getUser(), cart);
+        return orderMapper.toDto(orderRepository.save(order));
     }
 
     @Transactional
