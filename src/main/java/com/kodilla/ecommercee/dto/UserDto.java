60926a863e6fad730f4d835e6975bf88d9d3d01a
Anna-Dyrdul <81525080+Anna-Dyrdul@users.noreply.github.com>
2021-09-13 13:11:23 +0200

    task-25 UserController - rzeczywista implementacja metod (#34)
    * UserController - rzeczywista implementacja metod

--- src/main/java/com/kodilla/ecommercee/dto/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/UserDto.java
@@ -1,4 +1,20 @@
 package com.kodilla.ecommercee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class UserDto {
+
+    private long id;
+    private String username;
+    private boolean blocked;
+    private UserKeyDto userKey;
+    private List<Long> cartsId;
+    private List<Long> ordersId;
 }
